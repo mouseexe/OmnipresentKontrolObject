4fe4f4905ee52baf7994d9d76a95e88f20760d72
mouseexe <kay.s.schwenk@gmail.com>
2025-09-14 21:14:27 -0400

    Init commit

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    kotlin("jvm") version "2.2.0"
+    application
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+group = "gay.spiders"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("dev.kord:kord-core:0.13.1")
+    implementation("ch.qos.logback:logback-classic:1.4.11")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(22)
+}
+
+application {
+    mainClass.set("MainKt")
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,50 @@
+package gay.spiders
+
+import dev.kord.core.Kord
+import dev.kord.core.entity.Message
+import dev.kord.core.event.gateway.ReadyEvent
+import dev.kord.core.event.message.MessageCreateEvent
+import dev.kord.core.on
+import dev.kord.gateway.Intent
+import dev.kord.gateway.PrivilegedIntent
+import kotlinx.coroutines.runBlocking
+import org.slf4j.LoggerFactory
+
+private val logger = LoggerFactory.getLogger("gay.spiders.OmnipresentKontrolObject")
+
+fun main() = runBlocking {
+    logger.info("Bot is starting...")
+
+    val token = System.getenv("DISCORD_TOKEN")
+    if (token == null) {
+        println("Error: DISCORD_TOKEN environment variable not set.")
+        return@runBlocking
+    }
+
+    val kord = Kord(token)
+
+    kord.on<ReadyEvent> {
+        logger.info("Logged in as ${self.tag}")
+        logger.info("Connected to ${guilds.count()} guilds.")
+    }
+
+    kord.on<MessageCreateEvent> {
+        onMessage(message)
+    }
+
+    kord.login {
+        @OptIn(PrivilegedIntent::class)
+        intents += Intent.MessageContent
+    }
+
+    logger.info("Bot is shutting down.")
+}
+
+suspend fun onMessage(message: Message) {
+    if (message.author?.isBot == true) return
+
+    if (message.content == "!ping") {
+        logger.debug("Received !ping from ${message.author?.tag}")
+        message.channel.createMessage("Pong! 🏓")
+    }
+}
